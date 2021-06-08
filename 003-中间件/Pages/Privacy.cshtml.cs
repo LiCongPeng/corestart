c9fb368c452328cea9762cb207d2b6c2795a65b5
李从鹏 <licp@mingyuanyun.com>
2021-06-08 22:41:55 +0800

    添加项目文件。

---
+++ 003-中间件/Pages/Privacy.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _003_中间件.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ 配置框架/Program.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Memory;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+
+namespace 配置框架
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandLineConfigurationSourceDemo(args);
+
+            Console.ReadKey();
+        }
+
+        private static void MemoryConfigurationSourceDemo()
+        {
+            var source = new Dictionary<string, string>
+            {
+                ["DataFormat"] = "yyyy dd mm",
+                ["format:TimeFormat"] = "yyyy dd mm hh MM",
+
+            };
+
+            var config = new ConfigurationBuilder()
+                .Add(new MemoryConfigurationSource { InitialData = source })
+                .Build();
+
+            //var obj = config.GetSection("format").Get<FormatOption>();
+
+            Console.WriteLine(config["DataFormat"]);
+
+            Console.WriteLine(config.GetSection("format")["TimeFormat"]);
+
+        }
+
+        class FormatOption
+        {
+
+        }
+
+        private static void JsonConfigurationSourceDemo()
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsetting.json", optional: true, reloadOnChange: true).Build();
+
+            // 监听JSON文件的变更
+            // 一次性的 监听
+            var changeToken = config.GetReloadToken();
+            changeToken.RegisterChangeCallback(state =>
+            {
+                Console.WriteLine(config["DataFormat"]);
+            }, config);
+
+            // 订阅变更事件
+            ChangeToken.OnChange(() => config.GetReloadToken(), () =>
+            {
+                Console.WriteLine(config["DataFormat"]);
+                //Console.WriteLine(config.GetValue<string>("DataFormat"));
+            });
+        }
+
+        private static void EnvironmentSourceDemo()
+        {
+            // 设置环境变量  
+            Environment.SetEnvironmentVariable("TEST_GENDER", "Male");
+            Environment.SetEnvironmentVariable("TEST_AGE", "Male");
+
+            Environment.SetEnvironmentVariable("Name", "Male");
+
+            // 读取环境变量的值，以Test_ 开头的，只会读取到前两个
+            var config = new ConfigurationBuilder()
+                .AddEnvironmentVariables("TEST_")
+                .Build();
+
+            Console.WriteLine(config["GENDER"]);
+
+        }
+
+        private static void CommandLineConfigurationSourceDemo(string[] arg)
+        {
+            var map = new Dictionary<string, string>
+            {
+                ["-a"] = "argment"
+            };
+
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(arg, map).
+                Build();
+
+            Console.WriteLine("这是我从命令行读取的参数："+config["argment"]);
+        }
+
+    }
+}
